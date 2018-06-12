870d32832c7432c7999308cec7048861e1eed460
Administrator <981577040@qq.com>
2018-06-12 23:51:11 +0800

    crawl images360

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+import json
+# import pymysql
+# conn=pymysql.connect(host='localhost',user='root',password='123456',database='images')
+# cursor=conn.cursor() #拿到游标，即mysql >
+# #执行sql   增：
+# sql='insert into user1(user,password) VALUES (%s,%s)'
+# print(sql)
+# # rows = cursor.execute(sql,('xixi',123))  #插入一条记录
+# rows = cursor.executemany(sql,[('xixi',123),('aaa',456),('ttt',147)]) #插入多行记录
+# print('%s row in set (0.00 sec)'%rows)
+#
+# conn.commit() #提交到数据库
+# cursor.close()
+# conn.close()
+
+# a = 'asdasd'
+# print(a[1:])
+
